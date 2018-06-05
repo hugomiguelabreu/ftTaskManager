fa824a708fcc8963489f6b1f6bf9d3e478c4a6b0
markerstone <hugo.miguel.abreu@gmail.com>
2018-06-05 10:59:11 +0100

    Added scrapper

--- taskManager/src/main/java/Client/Client.java
+++ taskManager/src/main/java/Client/Client.java
@@ -1,8 +1,29 @@
 package Client;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<String> uniqueLinks = new ArrayList<>();
+        Document doc = Jsoup.connect(args[0]).get();
+        System.out.println(doc.title());
+
+        Elements links = doc.select("a");
+        for (Element link : links) {
+            String absHref = link.attr("abs:href"); // "http://jsoup.org/"
+            if(!uniqueLinks.contains(absHref) && !absHref.equals(""))
+                uniqueLinks.add(absHref);
+        }
+
+        for (String k : uniqueLinks)
+            System.out.println(k);
 
     }
 
