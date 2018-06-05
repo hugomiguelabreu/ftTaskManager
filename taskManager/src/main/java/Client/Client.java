83b008afcf310ca8c2cd988c1ceec8ff54cc8a40
markerstone <hugo.miguel.abreu@gmail.com>
2018-06-05 12:06:51 +0100

    Coded executor to handle processing

--- taskManager/src/main/java/Client/Client.java
+++ taskManager/src/main/java/Client/Client.java
@@ -1,29 +1,48 @@
 package Client;
 
+import Interfaces.Task;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        ArrayList<String> uniqueLinks = new ArrayList<>();
-        Document doc = Jsoup.connect(args[0]).get();
-        System.out.println(doc.title());
+        Task ts =  new TaskStub();
+        Executor e = new Executor(ts);
+        Scanner read = new Scanner(System.in);
+        String line;
 
-        Elements links = doc.select("a");
-        for (Element link : links) {
-            String absHref = link.attr("abs:href"); // "http://jsoup.org/"
-            if(!uniqueLinks.contains(absHref) && !absHref.equals(""))
-                uniqueLinks.add(absHref);
+        System.out.println("[1] New task");
+        System.out.println("[2] Execute tasks given by the server");
+        System.out.println("[3] Quit normally");
+        System.out.println("[4] Quit unexpected");
+
+        while ((line = read.nextLine()) != null){
+            int selected = Integer.parseInt(line);
+            switch (selected){
+                case 1:
+                    System.out.print("URL:");
+                    ts.addTask(read.nextLine());
+                    break;
+                case 2:
+                    e.start();
+                    break;
+                case 3:
+                    e.finish();
+                    e.join();
+                    System.exit(0);
+                    break;
+                case 4:
+                    System.exit(1);
+                    break;
+            }
         }
-
-        for (String k : uniqueLinks)
-            System.out.println(k);
 
     }
 
---
+++ taskManager/src/main/java/Client/Executor.java
@@ -0,0 +1,62 @@
+package Client;
+
+import Interfaces.Task;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Executor extends Thread {
+
+    private boolean stop;
+    private Task tc;
+
+
+    public Executor(Task t){
+        stop = false;
+        tc = t;
+    }
+
+    @Override
+    public void run() {
+        //Enquanto não houver sinal para parar
+        //de tratar pedidos vamos continuar;
+        while (!stop){
+            String newUrl = tc.getTask();
+            ArrayList<String> newTaks = scrappe(newUrl);
+            for (String k : newTaks) {
+                System.out.println(k);
+                tc.addTask(k);
+            }
+        }
+    }
+
+    public void finish(){
+        stop = true;
+    }
+
+    private ArrayList<String> scrappe(String url){
+        ArrayList<String> result = new ArrayList<>();
+        Document doc = null;
+
+        try {
+            doc = Jsoup.connect(url).get();
+            System.out.println(doc.title());
+
+            Elements links = doc.select("a");
+            for (Element link : links) {
+                String absHref = link.attr("abs:href"); // "http://jsoup.org/"
+                if(!result.contains(absHref) && !absHref.equals(""))
+                    result.add(absHref);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
