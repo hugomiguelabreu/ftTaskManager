86d0eabf7d922c548b2731507de33e4261ed6b8d
markerstone <hugo.miguel.abreu@gmail.com>
2018-06-16 22:40:34 +0100

    Fixed state recovery serializer

--- taskManager/src/main/java/Network/Spread/RecoverRep.java
+++ taskManager/src/main/java/Network/Spread/RecoverRep.java
@@ -1,9 +1,6 @@
 package Network.Spread;
 
-import Network.AddTasksRep;
-import Network.ClientUIDRep;
-import Network.CompleteTaskRep;
-import Network.GetTaskRep;
+import Network.*;
 import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.CatalystSerializable;
@@ -41,9 +38,8 @@ public class RecoverRep implements CatalystSerializable {
         bufferOutput.writeInt(responses.size());
         for(String r: responses.keySet()){
             bufferOutput.writeString(r);
-            bufferOutput.writeString(responses.get(r).getKey().getClass().getName());
-            serializer.writeObject(responses.get(r).getKey());
             bufferOutput.writeBoolean(responses.get(r).getValue().isDone());
+            serializer.writeObject(responses.get(r).getKey(), bufferOutput);
             //serializer.writeObject(responses.get(r).getValue());
         }
 
@@ -66,32 +62,11 @@ public class RecoverRep implements CatalystSerializable {
         size = bufferInput.readInt();
         for(int i = 0; i < size; i++) {
             String uid = bufferInput.readString();
-            String objectType = bufferInput.readString();
-            Object ob = null;
-            serializer.register(GetTaskRep.class);
-            serializer.register(ClientUIDRep.class);
-            serializer.register(CompleteTaskRep.class);
-            serializer.register(AddTasksRep.class);
-            switch (objectType) {
-                case "Network.AddTaskRep":
-                    AddTasksRep atr = serializer.readObject(bufferInput);
-                    ob = atr;
-                    break;
-                case "Network.CompleteTaskRep":
-                    CompleteTaskRep ctr = serializer.readObject(bufferInput);
-                    ob = ctr;
-                    break;
-                case "Network.ClientUIDRep":
-                    ClientUIDRep cuid = serializer.readObject(bufferInput);
-                    ob = cuid;
-                    break;
-                case "Network.GetTaskRep":
-                    GetTaskRep gt = serializer.readObject(bufferInput);
-                    ob = gt;
-                    break;
-            }
 
             boolean completedFuture = bufferInput.readBoolean();
+
+            Object ob = serializer.readObject(bufferInput);
+
             CompletableFuture cf = new CompletableFuture();
             //O futuro é suposto estar completo
             if(completedFuture)
